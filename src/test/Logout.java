3b45557555918e784684757d9f21d847707fd7d4
founasrahou <founasrahou@hotmail.com>
2020-02-29 12:56:18 -0800

    add locatingMap

--- src/test/Logout.java
+++ src/test/Logout.java
@@ -4,5 +4,6 @@ public class Logout {
     public static void main(String[] args) {
       //  System.out.println("log out");
        // update
+        // hello again from intelleji********
     }
 }
